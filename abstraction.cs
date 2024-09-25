67e7cfc056a8f38d4df87ae17523f2db5cf1be54
RAMANUJTIWARY <115652642+RAMANUJTIWARY@users.noreply.github.com>
2024-09-25 22:20:02 +0530

    Create abstraction.cs

---
+++ abstraction.cs
@@ -0,0 +1,8 @@
+Abstraction:- Hiding irrelevant data
+abstract class which contains the abstract keyword in his declaration 
+it can be used as a base class 
+abstract class may have abstract method or not
+abstract method:- abstract methood have no body only declaration in abstract class
+an abstract class we cannot create object of that class
+if we inherit abstract class then derived class must be defining the body of the abstarct method
+
